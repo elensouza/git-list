e6d5250ba16bf398ba37ac0fdaa9ab3bf54b78cf
Elen Souza <souzaelenn@gmail>
2024-06-09 19:36:33 -0300

    initial organization

---
+++ GitList/GitList/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitList
+//
+//  Created by Elen Souza on 09/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
